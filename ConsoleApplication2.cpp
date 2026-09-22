ce9de6dec9bbcd991b8965a89c608c04e73b69ac
Kilcelsnpl2 <kirillkarpov464@gmail.com>
2026-09-23 01:05:36 +0700

    1234

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() 
+{
+  double p, m, Q, K;
+  cout << "p = ";
+  cin >> p;
+  
+  cout << "m = ";
+  cin >> m;
+  
+  cout << "Q = ";
+  cin >> Q;
+  
+  cout << "K = ";
+  cin >> K;
+  
+  double n;
+  n = (K - 1) / K;
+  
+  //with full regeneration
+  double n1;
+  n1 = 1 - (1 / p);
+  
+  //with partial regeneration
+  double n2;
+  n2 = ((p - 1) * (pow(m, n) - 1)) / (pow(m, n) * (p - 1) - Q * (p - pow(m, n)));
+  
+  //in the absence of regeneration
+  double n3;
+  n3 = 1 - (1 / pow(m, n));
+  
+  
+  cout << "Efficiency with full regeneration = " << n1;
+  cout << "\nEfficiency with partial regeneration = " << n2;
+  cout << "\nEfficiency in the absence of regeneration = " << n3;
+}
